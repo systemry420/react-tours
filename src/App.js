b7b286a0834f0dfbcc595e1b888cfb8689216c10
hsn <systemry420gmail.com>
2020-11-06 19:27:32 +0200

    add a small ruler div below main title

--- src/App.js
+++ src/App.js
@@ -13,10 +13,21 @@ function App() {
       Line 1:26:   'useEffect' is defined but never used           no-unused-vars
       Line 8:17:   'setTours' is assigned a value but never used   no-unused-vars
       Line 18:11:  'fetchData' is assigned a value but never used  no-unused-vars`,
-      
+
       image: 'https://images.prismic.io/digitalocean/21ab4275-bcf3-42ac-b148-6f81d516ca1d_Twitter_graphic.png?auto=compress,format&rect=0,61,1024,387&w=370&h=140',
       price: '222'
-    }
+    },
+    {
+      id:1,
+      name: "abc",
+      info: `src/App.js
+      Line 1:26:   'useEffect' is defined but never used           no-unused-vars
+      Line 8:17:   'setTours' is assigned a value but never used   no-unused-vars
+      Line 18:11:  'fetchData' is assigned a value but never used  no-unused-vars`,
+
+      image: 'https://images.prismic.io/digitalocean/21ab4275-bcf3-42ac-b148-6f81d516ca1d_Twitter_graphic.png?auto=compress,format&rect=0,61,1024,387&w=370&h=140',
+      price: '222'
+    },
   ])
       
     const fetchData = () => {
@@ -36,6 +47,7 @@ function App() {
   return (
     <div className="App">
       <h1>Our Tours</h1>
+      <div className="ruler"></div>
       {tours.map(tour=>{
         return <Tour {...tour} key={tour.id}/>
       })}
